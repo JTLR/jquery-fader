22cb473f8d74cb80f64727c0b948337ac02c202f
Joe Taylor <joseph@emosaic.co.uk>
2014-08-19 10:53:26 +0100

    Fix scope

--- jquery.fader.js
+++ jquery.fader.js
@@ -1,35 +1,35 @@
 (function($) {
-  $.fn.fader = function(options) {
-    
-    // Plugin defaults
-    var settings = $.extend({
-      interval: 2000,
-      transitionSpeed: 500,
-      selector: '.fade-item'
-    }, options),
-        fader = this;
-        currentItem = $(settings.selector + ':first-child', fader),
-        nextItem = currentItem.next(settings.selector);
-    
-		if (nextItem.length < 1) {
-        nextItem = $(settings.selector + ':first-child', fader);
-      }
-    
-    var interval = setInterval(function() {
-      currentItem.fadeOut(settings.transitionSpeed);
-      nextItem.fadeIn(settings.transitionSpeed);
-      recalculateItems(nextItem);
-    }, settings.interval);
-    
-    function recalculateItems(prevNextItem) {
-      currentItem = prevNextItem;
-      nextItem = currentItem.next(settings.selector);
-      if (nextItem.length < 1) {
-        nextItem = $(settings.selector + ':first-child', fader);
-      }
-    }
-    
-    
-  return fader;
-  };
-} (jQuery));
+    $.fn.fader = function(options) {
+
+        // Plugin defaults
+        var settings = $.extend({
+            interval: 2000,
+            transitionSpeed: 500,
+            selector: '.fade-item'
+        }, options),
+                fader = this,
+                currentItem = $(settings.selector + ':first-child', fader),
+                nextItem = currentItem.next(settings.selector);
+
+        if (nextItem.length < 1) {
+            nextItem = $(settings.selector + ':first-child', fader);
+        }
+
+        var interval = setInterval(function() {
+            currentItem.fadeOut(settings.transitionSpeed);
+            nextItem.fadeIn(settings.transitionSpeed);
+            recalculateItems(nextItem);
+        }, settings.interval);
+
+        function recalculateItems(prevNextItem) {
+            currentItem = prevNextItem;
+            nextItem = currentItem.next(settings.selector);
+            if (nextItem.length < 1) {
+                nextItem = $(settings.selector + ':first-child', fader);
+            }
+        }
+
+        return fader;
+    };
+}(jQuery));
+
